8618f5a48c10c8d9722b1ee28a088ba142bb56e3
BoraYalcinn <borayalcintr@gmail.com>
2026-01-18 21:57:29 +0300

    its not working properly just yet

--- main.cpp
+++ main.cpp
@@ -42,11 +42,11 @@ int main() {
     int ambientLoc = GetShaderLocation(shader, "ambient");
     SetShaderValue(shader, ambientLoc, (float[4]){ 0.1f, 0.1f, 0.1f, 1.0f }, SHADER_UNIFORM_VEC4);
 
-    Light lights[3] = {0};
-    CreateLight(LIGHT_POINT,(Vector3){ 3, 3, 2 },Vector3Zero(),YELLOW,shader);
-    CreateLight(LIGHT_POINT,(Vector3){ -3, 3, 2 },Vector3Zero(),RED,shader);
-    CreateLight(LIGHT_POINT,(Vector3){ 3, -3, 2 },Vector3Zero(),GREEN,shader);
-    CreateLight(LIGHT_POINT,(Vector3){ -3, -3, 2 },Vector3Zero(),BLUE,shader);
+    Light lights[4] = {0};
+    lights[0] = CreateLight(LIGHT_POINT,(Vector3){ 3, 3, 2 },Vector3Zero(),YELLOW,shader);
+    lights[1] = CreateLight(LIGHT_POINT,(Vector3){ -3, 3, 2 },Vector3Zero(),RED,shader);
+    lights[2] = CreateLight(LIGHT_POINT,(Vector3){ 3, -3, 2 },Vector3Zero(),GREEN,shader);
+    lights[3] = CreateLight(LIGHT_POINT,(Vector3){ -3, -3, 2 },Vector3Zero(),BLUE,shader);
 
     SetTargetFPS(60);
 
@@ -69,7 +69,8 @@ int main() {
         BeginMode3D(camera);
         BeginShaderMode(shader);
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 4; i++) {
+            UpdateLightValues(shader, lights[i]);
             if (lights[i].enabled) {
                 DrawSphereEx(lights[i].position, lights[i].position.y, lights[i].position.z,8,lights[i].color);
             }else {
